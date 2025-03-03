f46d7a6c34a50f0b77912791208386c6ad266ff6
UwakmfonCharles <uchrles20@gmail.com>
2025-03-03 14:39:57 +0000

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -1,104 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankAccount
+namespace Polymorpism
 {
     public class Customer
-
     {
-
-        private string _FirstName;
-
-        private string _LastName;
-
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
         public int ID { get; private set; }
-
-        private static int NextID;
-
-
-
-
-
-        public string FirstName
-
-        {
-
-            get
-
-            {
-
-                return _FirstName;
-
-            }
-
-            private set
-
-            {
-
-                if (value != null && value.Length >= 1)
-
-                { _FirstName = value; }
-
-                else { throw new Exception("Invalid FirstName"); }
-
-            }
-
-        }
-
-
-
-
-
-        public string LastName
-
-        {
-
-            get
-
-            {
-
-                return _LastName;
-
-            }
-
-            private set
-
-            {
-
-                if (value != null && value.Length >= 1)
-
-                { _LastName = value; }
-
-                else { throw new Exception("Invalid LastName"); }
-
-            }
-
-        }
-
-
-
-
-
-        public string FullName { get { return FirstName + " " + LastName; } }
-
-
+        static int nextID = 1;
 
         public Customer(string firstName, string lastName)
-
         {
-
             FirstName = firstName;
-
             LastName = lastName;
-
-            ID = NextID;
-
-            NextID++;
-
+            ID = nextID;
+            nextID++;
+            
         }
 
+        
     }
 }
