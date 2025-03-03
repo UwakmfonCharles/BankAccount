2b657c7d729954412065fb7add402171a203f816
UwakmfonCharles <uchrles20@gmail.com>
2025-03-03 14:40:17 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,76 +1,46 @@
-﻿using BankAccount;
+using Polymorpism;
 
+Customer customer1 = new Customer("John", "Smith");
+Customer customer2 = new Customer("Mary", "Jones");
 
+Bank bank = new Bank();
 
-Customer customer1 = new Customer("Uwak", "Charles");
+bank.AddCustomer(customer1);
+bank.AddCustomer(customer2);
 
-Customer customer2 = new Customer("Liss", "James");
+Account johnAccount = new ChildAccount(customer1);
+Account maryAccount = new Account(customer2);
 
+bank.AddAccount(johnAccount);
+bank.AddAccount(maryAccount);
 
+Console.WriteLine(johnAccount);
+Console.WriteLine(maryAccount);
 
-Account account1 = new Account(customer1, 500);
+johnAccount.Deposit(15);
+maryAccount.Deposit(15);
 
-Account account2 = new Account(customer2, 500);
+Console.WriteLine(johnAccount);
+Console.WriteLine(maryAccount);
 
+bank.Transfer(johnAccount.ID, maryAccount.ID, 11); // this should fail - more than 10
 
+Console.WriteLine(johnAccount);
+Console.WriteLine(maryAccount);
 
-ChildAccount childAccount = new ChildAccount(customer1);
+bank.Transfer(johnAccount.ID, maryAccount.ID, 10); // this should pass
 
-Account account3 = new Account(customer1, 50);
+Console.WriteLine(johnAccount);
+Console.WriteLine(maryAccount);
 
-Customer anotherCustomer = new Customer("Jane", "Jones");
+bank.Transfer(johnAccount.ID, maryAccount.ID, 10); // this should fail - not enough funds
 
-CurrentAccount currentAccount = new CurrentAccount(anotherCustomer);
-currentAccount.Deposit(10);
+bank.Transfer(maryAccount.ID, johnAccount.ID, 11); // this should pass - maryAccount is not a ChildAccount
 
-if (currentAccount.Withdraw(10))
-{
-    Console.WriteLine("Able to withdraw 10");
-}
+Console.WriteLine(johnAccount);
+Console.WriteLine(maryAccount);
 
+bank.Transfer(maryAccount.ID, johnAccount.ID, 100); // this should fail - not enough funds
 
-Console.WriteLine($"Account ID: {account1.ID}, Customer: {account1.Customer.FullName}, Balance: {account1.Balance}");
-
-Console.WriteLine($"Account ID: {account2.ID}, Customer: {account2.Customer.FullName}, Balance: {account2.Balance}");
-
-Console.WriteLine($"Account ID {account3.ID}, Customer: {account3.Customer.FullName}, Balance: {account3.Balance}");
-
-
-
-
-
-// Test Deposit 
-
-Console.WriteLine("Depositing $50 into account1...");
-
-account1.Deposit(50);
-
-Console.WriteLine($"New Balance of Account {account1.ID}: {account1.Balance}");
-
-
-
-// Test Withdraw 
-
-Console.WriteLine("Withdrawing $30 from account2...");
-
-account2.Withdraw(30);
-
-Console.WriteLine($"New Balance of Account {account2.ID}: {account2.Balance}");
-
-
-
-Console.WriteLine("Withdrawing $5 from account3...");
-
-account3.Withdraw(5);
-
-Console.WriteLine($"New Balance of Account {account3.ID}: {account3.Balance}");
-
-
-
-// Test Withdrawal that exceeds balance 
-
-Console.WriteLine("Attempting to withdraw $300 from account1...");
-
-bool withdrawalSuccess = account1.Withdraw(300);
-
-Console.WriteLine($"Withdrawal successful? {withdrawalSuccess}. New Balance of Account {account1.ID}: {account1.Balance}");
+Console.WriteLine(johnAccount);
+Console.WriteLine(maryAccount);
